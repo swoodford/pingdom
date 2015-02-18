b33c609f1346ad084dfda85ae199ecd2fe07fe01
Shawn Woodford <sw@shawnwoodford.com>
2015-02-18 11:19:20 -0500

    This script lists creation date and time of all Pingdom checks in your account

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# pingdom
+pingdom
+=======
+
 Pingdom API tools
+
+[![Build Status](https://travis-ci.org/swoodford/pingdom.svg?branch=master)](https://travis-ci.org/swoodford/pingdom)
+
+- **pingdom-check-date-created.sh** This script lists creation date and time of all Pingdom checks in your account
---
+++ pingdom-check-date-created.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# This script lists creation date and time of all Pingdom checks in your account
+# Run the script and append its output to a .csv file
+# Requires jq (http://stedolan.github.io/jq/download/)
+
+# Set required variables
+EMAIL="Your Pingdom Email"
+PASSWORD="Your Pingdom Password"
+ACCOUNTEMAIL="Your Pingdom Group Account Email"
+APPKEY="Your Pingdom API App-Key"
+
+# Make sure the variables have been set
+if [ "$EMAIL" = "Your Pingdom Email" ]; then
+  echo "Must set variables before running."
+  exit 1
+fi
+
+# Test if jq already installed, else install it
+command -v jq >/dev/null 2>&1 || {
+  echo "Installing jq."
+  brew install jq
+  echo "jq installed."
+}
+
+# Get list of all checks
+CHECKS=$(curl -s --user $EMAIL:$PASSWORD https://api.pingdom.com/api/2.0/checks -H Account-Email:$ACCOUNTEMAIL -H App-Key:$APPKEY | jq . )
+
+# Count number of checks
+TOTALCHECKS=$(echo "$CHECKS" | grep -w id | wc -l)
+
+# Get list of check ID numbers
+CHECKIDS=$(echo "$CHECKS" | grep -w id | nl)
+
+START=1
+for (( COUNT=$START; COUNT<=$TOTALCHECKS; COUNT++ ))
+do
+  # echo \#$COUNT
+  
+  # Process one check at a time
+  CHECK=$(echo "$CHECKIDS" | grep -w $COUNT | cut -d : -f 2 | rev | cut -c 2- | rev | cut -c 2-)
+  # echo "Check ID: "$CHECK
+  
+  # Get detailed information on check
+  DETAILEDCHECK=$(curl -s --user $EMAIL:$PASSWORD https://api.pingdom.com/api/2.0/checks/$CHECK -H Account-Email:$ACCOUNTEMAIL -H App-Key:$APPKEY | jq . )
+  # echo "$DETAILEDCHECK"
+
+  # Get name of check
+  NAME=$(echo "$DETAILEDCHECK" | grep -m 1 -w name | cut -d '"' -f 4)
+  # echo "Check Name: "$NAME
+
+  # Get check created timestamp
+  CREATED=$(echo "$DETAILEDCHECK" | grep created | cut -d : -f 2 | rev | cut -c 2- | rev)
+
+  # Convert timestamp to human readable
+  DATECREATED=$(date -r $CREATED)
+  # echo $DATECREATED
+
+  # Output data in CSV format  
+  echo $DATECREATED,$NAME
+done
