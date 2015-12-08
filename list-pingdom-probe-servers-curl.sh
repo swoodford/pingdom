c9bad23df43b908304269eecab89c8a360141b77
Shawn Woodford <sw@shawnwoodford.com>
2015-12-08 18:21:24 -0500

    check for curl

--- list-pingdom-probe-servers-curl.sh
+++ list-pingdom-probe-servers-curl.sh
@@ -5,6 +5,13 @@
 # More info:
 # https://support.pingdom.com/Knowledgebase/Article/View/16/0/where-can-i-find-a-list-of-ip-addresses-for-the-pingdom-probe-servers
 
+function check_command {
+	type -P $1 &>/dev/null || fail "Unable to find $1, please install it and run this script again."
+}
+
+# Check required commands
+check_command "curl"
+
 curl -s https://my.pingdom.com/probes/feed | \
 grep "pingdom:ip" | \
 sed -e 's|</.*||' -e 's|.*>||' | \
